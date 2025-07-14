8140e77c17a13619e0ab78180e847a458def8e65
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-07-14 23:15:55 +0530

    style: Enhance CategorySelector UI with improved gradients and animations

--- lib/presentation/widgets/category_selector.dart
+++ lib/presentation/widgets/category_selector.dart
@@ -18,49 +18,93 @@ class CategorySelector extends StatelessWidget {
       physics: const NeverScrollableScrollPhysics(),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 4,
-        childAspectRatio: 1,
-        crossAxisSpacing: 8,
-        mainAxisSpacing: 8,
+        childAspectRatio: 0.85,
+        crossAxisSpacing: 12,
+        mainAxisSpacing: 12,
       ),
       itemCount: Categories.expenseCategories.length,
       itemBuilder: (context, index) {
         final category = Categories.expenseCategories[index];
         final isSelected = category.name == selectedCategory;
 
-        return InkWell(
-          onTap: () => onCategorySelected(category.name),
-          borderRadius: BorderRadius.circular(12),
-          child: Container(
-            decoration: BoxDecoration(
-              color: isSelected
-                  ? category.color.withOpacity(0.2)
-                  : Colors.grey.withOpacity(0.1),
-              borderRadius: BorderRadius.circular(12),
-              border: Border.all(
-                color: isSelected ? category.color : Colors.transparent,
-                width: 2,
+        return Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: () => onCategorySelected(category.name),
+            borderRadius: BorderRadius.circular(16),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 200),
+              decoration: BoxDecoration(
+                gradient: isSelected
+                    ? LinearGradient(
+                        colors: [
+                          category.color.withOpacity(0.8),
+                          category.color,
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      )
+                    : LinearGradient(
+                        colors: [
+                          Colors.grey.shade50,
+                          Colors.grey.shade100,
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                borderRadius: BorderRadius.circular(16),
+                border: Border.all(
+                  color: isSelected
+                      ? category.color.withOpacity(0.5)
+                      : Colors.grey.withOpacity(0.2),
+                  width: isSelected ? 2 : 1,
+                ),
+                boxShadow: isSelected
+                    ? [
+                        BoxShadow(
+                          color: category.color.withOpacity(0.3),
+                          blurRadius: 8,
+                          offset: const Offset(0, 4),
+                        ),
+                      ]
+                    : [],
               ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  category.icon,
-                  color: isSelected ? category.color : Colors.grey[600],
-                  size: 24,
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: isSelected
+                            ? Colors.white.withOpacity(0.2)
+                            : category.color.withOpacity(0.1),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Icon(
+                        category.icon,
+                        color: isSelected ? Colors.white : category.color,
+                        size: 24,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      category.name.length > 8
+                          ? '${category.name.substring(0, 8)}...'
+                          : category.name,
+                      style: TextStyle(
+                        fontSize: 11,
+                        color: isSelected ? Colors.white : Colors.grey[700],
+                        fontWeight: isSelected ? FontWeight.bold : FontWeight.w500,
+                      ),
+                      textAlign: TextAlign.center,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 4),
-                Text(
-                  category.name.split(' ').first,
-                  style: TextStyle(
-                    fontSize: 10,
-                    color: isSelected ? category.color : Colors.grey[600],
-                    fontWeight:
-                        isSelected ? FontWeight.bold : FontWeight.normal,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
+              ),
             ),
           ),
         );
